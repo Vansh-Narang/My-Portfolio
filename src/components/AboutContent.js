7293e115ef36f4678d6469f6d7f4c9516715a9c9
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 23:13:54 +0530

    About Page Styling Done

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -7,14 +7,15 @@ import p2 from "../assets/p2.jpg";
 const AboutContent = () => {
     return (
         <div className="about">
-            <div className="left"></div>
-            <h1>Who am I?</h1>
-            <p>I am Developer</p>
-            <Link to="/contact">
-                <button className="btn">
-                    Contact
-                </button>
-            </Link>
+            <div className="left">
+                <h1>Who am I?</h1>
+                <p>I am Developer</p>
+                <Link to="/contact">
+                    <button className="btn">
+                        Contact
+                    </button>
+                </Link>
+            </div>
             <div className="right">
                 <div className="img-container">
                     <div className="img-stack top">
