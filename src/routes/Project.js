484e8567b23c34735fda81a355032f846171fc03
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 22:11:17 +0530

    Added Props In Project Page

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from '../components/footer';
+import HeroImg2 from '../components/HeroImg2';
 const Project = () => {
     return (
         <div>
             <Navbar />
+            <HeroImg2 heading="Projects" text="Some of my most recent works" />
             <Footer />
         </div>
     )
