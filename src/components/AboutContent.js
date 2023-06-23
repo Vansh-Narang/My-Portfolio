03c64b2575331e525a065c8a890ff10ea7d5e671
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 23:06:26 +0530

    About Js Added

---
+++ src/components/AboutContent.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./AboutContentStyle.css";
+import React from 'react'
+import p1 from "../assets/p1.jpg";
+import p2 from "../assets/p2.jpg";
+
+const AboutContent = () => {
+    return (
+        <div className="about">
+            <div className="left"></div>
+            <h1>Who am I?</h1>
+            <p>I am Developer</p>
+            <Link to="/contact">
+                <button className="btn">
+                    Contact
+                </button>
+            </Link>
+            <div className="right">
+                <div className="img-container">
+                    <div className="img-stack top">
+                        <img src={p1} className="img" alt="image"></img>
+                    </div>
+                    <div className="img-stack bottom">
+                        <img src={p2} className="img" alt="image"></img>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default AboutContent
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from '../components/footer';
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 const About = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="ABOUT" text="Im a friendly Developer" />
+      <AboutContent />
       <Footer />
     </div>
   )
