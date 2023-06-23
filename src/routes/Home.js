9306ecab33dd2a26b9fe97d48cd5cdc5ce1182f8
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 23:24:39 +0530

    Completed the Video

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Work from "../components/Work"
 import HeroImage from '../components/HeroImage';
 import Navbar from "../components/Navbar";
 import Footer from '../components/footer';
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Navbar />
             <HeroImage />
+            <Work />
             <Footer />
         </div >
     )
