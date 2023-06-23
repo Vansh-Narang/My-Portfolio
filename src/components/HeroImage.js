2bbfd1daaae62f31385756489c287d9a5319ca79
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 18:33:59 +0530

    Assets Added

---
+++ src/components/HeroImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroImageStyle.css'
+
+const HeroImage = () => {
+    return (
+        <div className='hero'>
+            <div className='mask'>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default HeroImage
