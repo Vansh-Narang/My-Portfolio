4a8162526197a56f4fa54330fab37b4bdb43d9c6
Vansh=Narang <vanshnarang49906@gmail.com>
2023-06-23 18:59:18 +0530

    ScrollY

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
     const [color, setColor] = useState(false)
     const changeColor = () => {
-        if (window.scrollY >= 1) {
+        if (window.scrollY >= 100) {
             setColor(true)
         }
         else {
